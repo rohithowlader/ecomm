fbe24c6fb714aff59835567644c7f0a42dcab6d8
Rohit Howlader <rohithowlader2017@gmail.com>
2025-05-25 02:52:36 +0530

    commented code use only if cart or admin portal is setting req.user.id

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,3 +16,21 @@ module.exports = function (req, res, next) {
     res.status(401).json({ message: "Invalid token" });
   }
 };
+
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+
+// const auth = async (req, res, next) => {
+//   const token = req.header("Authorization")?.replace("Bearer ", "");
+//   if (!token) return res.status(401).json({ message: "No token provided" });
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = { id: decoded.id };
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ message: "Invalid token" });
+//   }
+// };
+
+// module.exports = auth;
